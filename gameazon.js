94660d3b062a1d9f9a5c39a225449fe99465dbad
ravi-maithrey <63500743+ravi-maithrey@users.noreply.github.com>
2020-04-14 00:22:45 +0530

    Add files via upload

---
+++ gameazon.js
@@ -0,0 +1,21 @@
+let express = require("express");
+let app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(function(req, res){
+    res.type('text/plain');
+    res.status(404);
+    res.send('404 - page not found');
+});
+
+app,use(function(err, req, res, next){
+    console.error(err.stack);
+    res.type('text/plain');
+    res.status(500);
+    res.send('500 - internal server error');
+});
+
+app.listen(app.get('port'), function(){
+    console.log('Webpage started on http://localhost' + app.get('port') + ' ; Press Ctrl + C to terminate');
+});
